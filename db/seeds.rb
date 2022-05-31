5abee49a0054d508e76810c09f5563dc295772a3
Alex Wu <alexander.wu14@gmail.com>
2022-05-31 15:26:24 -0500

    initial seeds (manual) and cloudinary photo upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating users and items"
+
+default_user = User.create!(
+  email: "user@user.com",
+  password: "123456",
+  first_name: "Diego",
+  last_name: "Rivera",
+  phone_number: "888 326 9937",
+  address: "Av. Álvaro Obregón 213, Roma Nte., Cuauhtémoc, 06700 Ciudad de México, CDMX"
+)
+
+egg = Item.new(
+  user: default_user,
+  title: "Large brown eggs",
+  description: "Perfectly good brown eggs I bought last week. I'll be traveling and don't want to waste them.",
+  start_pickup_at: DateTime.strptime("06/10/2022 8:00", "%m/%d/%Y %H:%M"),
+  end_pickup_at: DateTime.strptime("06/12/2022 20:00", "%m/%d/%Y %H:%M"),
+  latitude: rand(19.20..19.60),
+  longitude: rand(-99.30..-98.90)
+)
+image_url = "https://source.unsplash.com/wJbDzUQoFBQ"
+downloaded_image = URI.open(image_url)
+egg.photo.attach(io: downloaded_image, filename: "egg-#{egg.id}")
+egg.save
+
+garlic = Item.new(
+  user: default_user,
+  title: "Garlic, peppers, tomato, etc.",
+  description: "Some leftover produce I won't get to. Please take them.",
+  start_pickup_at: DateTime.strptime("06/10/2022 8:00", "%m/%d/%Y %H:%M"),
+  end_pickup_at: DateTime.strptime("06/14/2022 20:00", "%m/%d/%Y %H:%M"),
+  latitude: rand(19.20..19.60),
+  longitude: rand(-99.30..-98.90)
+)
+image_url = "https://source.unsplash.com/AeKuux5keaU"
+downloaded_image = URI.open(image_url)
+garlic.photo.attach(io: downloaded_image, filename: "garlic-#{garlic.id}")
+garlic.save
+
+cauliflower = Item.new(
+  user: default_user,
+  title: "Cauliflower",
+  description: "Just a regular head of cauliflower",
+  start_pickup_at: DateTime.strptime("06/10/2022 8:00", "%m/%d/%Y %H:%M"),
+  end_pickup_at: DateTime.strptime("06/13/2022 20:00", "%m/%d/%Y %H:%M"),
+  latitude: rand(19.20..19.60),
+  longitude: rand(-99.30..-98.90)
+)
+image_url = "https://source.unsplash.com/0XVrBLy73rw"
+downloaded_image = URI.open(image_url)
+cauliflower.photo.attach(io: downloaded_image, filename: "cauliflower-#{cauliflower.id}")
+cauliflower.save
+
+potato = Item.new(
+  user: default_user,
+  title: "Bag of potatoes",
+  description: "Bag of small potatoes purchased a couple weeks ago",
+  start_pickup_at: DateTime.strptime("06/10/2022 8:00", "%m/%d/%Y %H:%M"),
+  end_pickup_at: DateTime.strptime("06/13/2022 20:00", "%m/%d/%Y %H:%M"),
+  latitude: rand(19.20..19.60),
+  longitude: rand(-99.30..-98.90)
+)
+image_url = "https://source.unsplash.com/_SHdelGzAp8"
+downloaded_image = URI.open(image_url)
+potato.photo.attach(io: downloaded_image, filename: "potato-#{potato.id}")
+potato.save
