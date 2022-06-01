50b36245c05f631f9683dd91770c663521c0bc5c
Alex Wu <alexander.wu14@gmail.com>
2022-06-01 12:09:50 -0500

    user route/controller/views and cloudinary refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   has_many :items
   has_many :pickups
   has_many :sharer_pickups, through: :items, source: :pickups
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def photo_or_default
+    photo.attached? ? photo.key : "default_avatar"
+  end
 end
