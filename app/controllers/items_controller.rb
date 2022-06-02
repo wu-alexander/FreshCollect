ef7686870ee005d22c0dfa46aa6b39e3193d968e
Alex Wu <alexander.wu14@gmail.com>
2022-06-02 14:11:49 -0500

    added markers, styling, etc.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,8 @@ class ItemsController < ApplicationController
     @markers = @items.geocoded.map do |item|
       {
         lat: item.latitude,
-        lng: item.longitude
+        lng: item.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { item: item })
       }
     end
   end
