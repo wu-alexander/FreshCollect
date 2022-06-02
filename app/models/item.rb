c9a2179038993791ce2caeac6e6644fe74095044
Alex Wu <alexander.wu14@gmail.com>
2022-06-02 12:37:03 -0500

    added initial geocoder, markers TBD

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
+  delegate :address, to: :user, prefix: true
+  geocoded_by :user_address
+  before_create :geocode, unless: :geocoded? # the instance method from geocoder
+
   def photo_or_default
     photo.attached? ? photo.key : "default_item"
   end
