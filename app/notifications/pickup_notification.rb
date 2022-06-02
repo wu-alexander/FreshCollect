a3ed4285eab86e5c927bc5eda8da6dc2e2f924d4
Sina Beeler <sina.beeler@gmail.com>
2022-06-02 15:00:19 -0500

    pickup notification

--- app/notifications/pickup_notification.rb
+++ app/notifications/pickup_notification.rb
@@ -1,15 +1,24 @@
 class PickupNotification < Noticed::Base
   deliver_by :database
+  deliver_by :action_cable, format: :to_websocket
 
   param :pickup
 
-  # Define helper methods to make rendering easier.
-  #
-  # def message
-  #   t(".message")
-  # end
-  #
-  # def url
-  #   post_path(params[:post])
-  # end
+  def text
+    "Yay! Your #{pickup.item.title} has just been booked!"
+  end
+
+  def path
+    pickup
+  end
+
+  def pickup
+    params[:pickup]
+  end
+
+  def to_websocket
+    {
+      popup_html: ApplicationController.render(partial: "notifications/notification", locals: { notification: self })
+    }
+  end
 end
