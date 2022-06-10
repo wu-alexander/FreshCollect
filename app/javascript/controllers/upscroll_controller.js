103c0213099476591e22afa1135612dee4e90d52
Fiona Feichtinger <fiona.feichtinger@gmail.com>
2022-06-10 13:33:40 -0500

    itemshow

--- app/javascript/controllers/upscroll_controller.js
+++ app/javascript/controllers/upscroll_controller.js
@@ -2,8 +2,9 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
+    window.scrollTo(0, 0)
     setTimeout(() => {
       window.scrollTo(0, window.innerWidth / 2)
-    }, 100);
+    }, 200);
   }
 }
